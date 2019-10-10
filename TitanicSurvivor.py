72ebcb77de6e8f0f6ebc5ba31351306bf5c22737
Lawrence Glenn <Lawrence.O.Glenn@gmail.com>
2019-10-09 18:54:09 -0700

    creature function to use label encorder to covert tables

--- TitanicSurvivor.py
+++ TitanicSurvivor.py
@@ -77,3 +77,16 @@ sns.barplot(x="Age", y="Survived", hue="Sex", data=data_train);
 sns.barplot(x="Cabin", y="Survived", hue="Sex", data=data_train);
 
 sns.barplot(x="Fare", y="Survived", hue="Sex", data=data_train);
+
+
+#function to use LabelEncoder to convert tables into more usable form
+def encode_features(df_train, df_test):
+    features = ['Fare', 'Cabin', 'Age', 'Sex', 'Lname', 'NamePrefix']
+    df_combined = pd.concat([df_train[features], df_test[features]])
+    
+    for feature in features:
+        le = preprocessing.LabelEncoder()
+        le = le.fit(df_combined[feature])
+        df_train[feature] = le.transform(df_train[feature])
+        df_test[feature] = le.transform(df_test[feature])
+    return df_train, df_test
